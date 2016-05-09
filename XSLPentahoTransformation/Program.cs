9e7e52677ef5c933ee8f46a854e3ce7ff5128d96
arielmanka <ariel.manka@gmail.com>
2016-05-09 13:15:26 +0400

    XSLPentahoTransformation

---
+++ XSLPentahoTransformation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Xsl;
+
+namespace XSLTransformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XslCompiledTransform myXslCompiledTransform;
+            myXslCompiledTransform = new XslCompiledTransform();
+            myXslCompiledTransform.Load(args[1]);
+            myXslCompiledTransform.Transform(args[0], args[2]);
+        }
+    }
+}
